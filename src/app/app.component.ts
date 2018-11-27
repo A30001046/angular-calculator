35b366fd4d6a8decce0d12453c78561d7c4d0bce
Dushan De Silva <dushand@gmail.com>
2018-11-28 10:30:55 +1100

    Description updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'E-CENTRIC INNOVATIONS CALCULATOR';
+  title = 'INNOVATIONS CALCULATOR';
 }
